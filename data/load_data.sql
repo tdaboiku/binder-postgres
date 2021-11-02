6938421524262f8c09a0b334167220ed88309eca
tdaboiku <72985336+tdaboiku@users.noreply.github.com>
2021-11-01 21:57:36 -0400

    Update and rename load_movies.sql to load_data.sql

---
+++ data/load_data.sql
@@ -0,0 +1,17 @@
+DROP TABLE if exists HfTaX;
+
+CREATE TABLE HfTaX(
+	Time decimal,
+	HfTa decimal,
+	HfTaNb decimal,
+	HfTaW decimal,
+	HfTaMo decimal);
+
+INSERT INTO HfTaX(Time, HfTa, HfTaNb, HfTaW, HfTaMo)
+VALUES (0, 0, 0, 0, 0),
+		(.17, 22.70, 131.70, 22.99, 13.51),
+		(.5, 27.38, 186.09, 43.60, 23.63),
+		(1, 41.87, 227.65, 30.30, 32.06 )
+		
+	
+SELECT * FROM HfTaX
